858fff7d5ae816b365c47af76e6bcb3b74d945c0
Golam Mahmud Rafi <rafi.aust@live.com>
2020-10-23 10:06:01 +0900

    Shopify library integrated

---
+++ app/Http/Controllers/SyncController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\sync;
+use Illuminate\Http\Request;
+
+class SyncController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\sync  $sync
+     * @return \Illuminate\Http\Response
+     */
+    public function show(sync $sync)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\sync  $sync
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(sync $sync)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\sync  $sync
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, sync $sync)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\sync  $sync
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(sync $sync)
+    {
+        //
+    }
+}
