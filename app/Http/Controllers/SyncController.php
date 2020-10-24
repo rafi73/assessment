57fa65687fac08d1575df8742a52192e5f8b6592
Golam Mahmud Rafi <rafi.aust@live.com>
2020-10-24 15:18:55 +0900

    Testing routes

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\sync;
 use Illuminate\Http\Request;
+use Response;
 
 class SyncController extends Controller
 {
@@ -12,9 +13,27 @@ class SyncController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function productUpdate(Request $request)
     {
-        //
+        // define('SHOPIFY_APP_SECRET', '*****************my_shopify_secret**********');
+
+        // $hmac_header = $_SERVER['HTTP_X_SHOPIFY_HMAC_SHA256'];
+        // $data = file_get_contents('php://input');
+        // $verified = verify_webhook($data, $hmac_header);
+        // error_log('Webhook verified: '.var_export($verified, true)); //check error.log to see the result*/
+
+        if (Request::header('X-Shopify-Hmac-Sha256')) {
+            $hmac_header = Request::header('X-Shopify-Hmac-Sha256');
+            $data = file_get_contents('php://input');
+            $calculated_hmac = base64_encode(hash_hmac('sha256', $data, Config::get('constants.SHOPIFY_APP_SECRET'), true));
+            if ($hmac_header != $calculated_hmac) {
+                return Response::json(array('error' => true, 'message' => "invalid secret"), 403);
+            }
+        } 
+        else 
+        {
+            return Response::json(array('error' => true, 'message' => "no secret"),  403);
+        }
     }
 
     /**
@@ -22,9 +41,9 @@ class SyncController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function index()
     {
-        //
+        return Response::json('test',  403);
     }
 
     /**
@@ -81,5 +100,11 @@ class SyncController extends Controller
     public function destroy(sync $sync)
     {
         //
+    }
+
+    public function verify_webhook($data, $hmac_header)
+    {
+        $calculated_hmac = base64_encode(hash_hmac('sha256', $data, SHOPIFY_APP_SECRET, true));
+        return hash_equals($hmac_header, $calculated_hmac);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'v1'], function ($router) {
+    Route::get('test', 'App\Http\Controllers\SyncController@index');
+    // Route::get('chapter-headings/{id}', 'App\Http\Controllers\ChapterHeadingController@show');
+    // Route::post('chapter-headings', 'App\Http\Controllers\ChapterHeadingController@create');
+    // Route::put('chapter-headings/{id}', 'App\Http\Controllers\ChapterHeadingController@update');
+    // Route::delete('chapter-headings/{id}', 'App\Http\Controllers\ChapterHeadingController@delete');
+    Route::post('update-product', 'App\Http\Controllers\SyncController@productUpdate');
+    // Route::get('chapter-headings-front', 'App\Http\Controllers\ChapterHeadingController@getChapterHeadings');
+
+    // Route::get('code-categories', 'App\Http\Controllers\CodeCategoryController@getCodeCategories');
+    // Route::get('suppliers', 'App\Http\Controllers\SupplierController@getSuppliers');
+    // Route::get('countries', 'App\Http\Controllers\CountryController@getCountries');
+    // Route::get('exports/{chapterHeadingId}', 'App\Http\Controllers\ExportController@getExportData');
+    // Route::get('exports-by-country/{chapterHeadingId}/{countryId}', 'App\Http\Controllers\ExportController@getExportDataByCountry');
+});
