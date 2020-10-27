7505e3b6d3b98d687e2f293813081ee2f7ff5e99
Golam Mahmud Rafi <rafi.aust@live.com>
2020-10-27 13:20:01 +0900

    Fix Webhook

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -11,12 +11,12 @@ class WebhookController extends Controller
 {
     public function __construct()
     {
-        define('SHOPIFY_APP_SECRET', env('WEBHOOK_SHARED_SECRET'));
+        // define('SHOPIFY_APP_SECRET', env('WEBHOOK_SHARED_SECRET'));
 
-        $hmac_header = $_SERVER['HTTP_X_SHOPIFY_HMAC_SHA256'];
-        $data = file_get_contents('php://input');
-        $verified = verify_webhook($data, $hmac_header);
-        error_log('Webhook verified: ' . var_export($verified, true));
+        // $hmac_header = $_SERVER['HTTP_X_SHOPIFY_HMAC_SHA256'];
+        // $data = file_get_contents('php://input');
+        // $verified = verify_webhook($data, $hmac_header);
+        // error_log('Webhook verified: ' . var_export($verified, true));
     }
 
     public function verify_webhook($data, $hmac_header)
